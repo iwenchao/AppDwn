f4246c54e3b7dab96d9a92f31ad9047b9243e440
wenchao <iwenchaos6688@163.com>
2024-06-05 15:54:09 +0800

    一个方便使用的安卓下载库

---
+++ app/src/main/java/com/chaos/down/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chaos.down
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
